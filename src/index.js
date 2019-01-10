7622261ceabf145d5c140b7e3456690c2b3a8d7a
Rahul Sahay <rahulsahay19@hotmail.com>
2019-01-10 20:09:37 +0530

    components understanding

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from './Spinner';
+import { className } from "postcss-selector-parser";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  //one liner is doing the ctor job
+ state = {lat: null,errorMsg:'' };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  //ComponentLifyCycle
+  componentDidMount(){
+    window.navigator.geolocation.getCurrentPosition(
+        position => this.setState({ lat: position.coords.latitude }),
+        err => this.setState({ errorMsg: err.message })
+        
+      );
+  }
+  componentDidUpdate(){
+    console.log('Component updated!');
+  }
+
+  renderContent(){
+    if(this.state.errorMsg && !this.state.lat){
+      return <div>Error: {this.state.errorMsg}</div>;
+  }
+
+  if(!this.state.errorMsg && this.state.lat){
+      return <SeasonDisplay lat={this.state.lat}/>
+  }
+  return <Spinner message="Please accept location request!"/>;
+  }
+
+  //render gets called almost all the time
+  render() {
+  
+  return (
+    <div className="border red">
+      {this.renderContent()}
+    </div>
+  );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
